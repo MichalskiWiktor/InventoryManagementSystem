70051fdfea0704fa627397ad03b91ebe4e02129f
wiktor <wiktor.kasa1@gmail.com>
2023-10-10 12:03:15 +0200

    updated product module

--- inventory/models.py
+++ inventory/models.py
@@ -16,16 +16,22 @@ class Category(models.Model):
         return self.name
 
 class Product(models.Model):
-    image = models.ImageField(upload_to='product_images/')
+    # Mandatory Fields
     name = models.CharField(max_length=50) 
     category = models.ManyToManyField(Category) 
-    selling_price = models.FloatField() 
-    manufacturing_price = models.FloatField() 
     stock = models.IntegerField()
-    brand = models.CharField(max_length=50) 
-    tags = models.ManyToManyField(Tag) 
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified_at = models.DateTimeField(auto_now=True)
+
+    # Non Mandatory Fields
+    image = models.ImageField(upload_to='product_images/', blank=True, null=True)
+    sku = models.CharField(max_length=50, blank=True, null=True) 
+    barcode = models.IntegerField(blank=True, null=True) 
+    brand = models.CharField(max_length=50, blank=True, null=True) 
+    tags = models.ManyToManyField(Tag, blank=True, null=True) 
+    exp_date= models.DateField(auto_now=False, blank=True, null=True)
+    selling_price = models.FloatField(blank=True, null=True) 
+    manufacturing_price = models.FloatField(blank=True, null=True) 
 
     def __str__(self):
         return self.name
